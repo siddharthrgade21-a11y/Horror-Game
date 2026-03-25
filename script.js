467c69610fe5e32b056f59bbaacec8cd82af89b1
Siddharth Gade <siddharthrgade21@gmail.com>
2026-03-25 13:11:09 +0530

    Horror Game
    An interactive horror game made with HTML, CSS, and JavaScript. The player reads a suspense story, selects choices, and hears real sound effects like footsteps and doors to create a realistic horror experience in the browser. 👻🎮

---
+++ script.js
@@ -0,0 +1,14 @@
+function choice1(){
+    document.getElementById("story").innerHTML =
+    "You open the door slowly... The hallway light starts blinking... Someone is standing at the end of the corridor.";
+}
+
+function choice2(){
+    document.getElementById("story").innerHTML =
+    "You hide under the blanket. Suddenly you feel someone sitting on your bed.";
+}
+
+function choice3(){
+    document.getElementById("story").innerHTML =
+    "You look through the window... Your reflection is smiling but you are not.";
+}
